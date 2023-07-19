1ae2c50578eb0a44fc518d65a070ce5eb61a23d6
maikol17s <maikol8382007@gmail.com>
2023-07-19 09:55:26 -0500

    17/07/2023

--- comprension1.py/excep6.py
+++ comprension1.py/excep6.py
@@ -1,17 +1,34 @@
-import random
-def listaError():
+def listasExcepciones():
+    lista = []
+    try:
+        while True:
+            elemento = float(input('Ingrese un elemento para añadir a la lista: '))
+            if elemento in lista:
+                print('No se puede agregar un elemento duplicado. Intente nuevamente')
+                print()
+            else:
+                lista.append(elemento)
+    except ValueError:
+        print('No se ha podido ingresar el elemento a la lista')
+    except KeyboardInterrupt:
+        print('Ha terminado de agregar elementos a la lista')
+    except:
+        print('Ha ocurrido un error')
+    print(f'LISTA: {lista}')
+    print()
+
     while True:
-        list=[]
-        tam=random.randrange(5,9)
-        list.append(tam)
-        for i in range(tam):
-            try:
-                x=int(input('Escriba un indice de la lista: '))
-                print('Imposible encontrar ese indice ')
-            except IndexError:
-                print('Indece no encontrad')
-            except ValueError:
-               print('Valor no valido')
-        
-        print('Fin')
-print(listaError())
+        try:
+            posicion = int(input('Ingrese la posicion que desea buscar en la lista: '))
+            elemento = lista[posicion]
+            print(f'En la posición {posicion} se encuentra el elemento {elemento}.')
+            print('--- FIN ---')
+            break
+        except IndexError:
+            print('La posición que desea encontrar no existe.')
+        except ValueError:
+            print('Ingrese un valor entero valido.')
+        except:
+            print('Ah ocurrido un error.')
+
+listasExcepciones()
--- comprension1.py/excep7.py
+++ comprension1.py/excep7.py
@@ -1,1 +1,31 @@
-dic={}
+def diccionarioExcepciones():
+    dic = {}
+    try:
+        while True:
+            clave = input('Ingrese una clave: ')
+            if clave in dic:
+                print('No se puede agregar una clave duplicada')
+                print()
+            else:
+                valor = input('Ingrese el valor de la clave:')
+                dic[clave] = valor
+    except KeyboardInterrupt:
+        print('\nHa terminado de agregar elementos al diccionario.')
+    except:
+        print('Ha ocurrido un error.')
+    print(f'DICCIONARIO: {dic}')
+    
+    print()
+
+    while True:
+        try:
+            clave_buscada = input('Ingrese la clave que desea buscar en el diccionario: ')
+            valor_encontrado = diccionario[clave_buscada]
+            print(f'La clave {clave_buscada} tiene el valor {valor_encontrado}.')
+        except KeyError:
+            print('La clave que desea encontrar no existe.')
+        except:
+            print('Ha ocurrido un error.')
+        print('--- FIN ---')
+
+diccionarioExcepciones()
