6334fb5777ad93e2b27b30ef00d064ffb88db0bf
maikol17s <maikol8382007@gmail.com>
2023-07-31 14:05:55 -0500

    modulo4

--- laboratorio1.py
+++ laboratorio1.py
@@ -1,17 +1,34 @@
+
 from os import strerror
 
-f1 = {chr(ch): 0 for ch in range(ord('a'), ord('z') + 2)}
-f2= input("Ingresa el nombre del archivo a analizar: ")
+counters = {chr(ch): 0 for ch in range(ord('a'), ord('z') + 1)}
+
+
+numeros_counter = 0
+caracteres_counter = 0
+caracteres_especiales = "{[/<¡(`^)%;>'}-@#$*,_!+|¿:].?"
+file_name = input("Ingresa el nombre del archivo a analizar: ")
+
+
 try:
-    file = open(f2, "rt")
-    for line in file:
-        for char in line:
-            if char.isalpha():
-                f1[char.lower()] += 1
-    file.close()
-    for char in f1.keys():
-        b = f1[char]
-        if b > 0:
-            print(char, '->', b)
+    with open(file_name, "rt") as file:
+        for line in file:
+            for char in line:
+                if char.isalpha():
+                    counters[char.lower()] += 1
+                elif char.isdigit():
+                    numeros_counter += 1
+                elif char in caracteres_especiales:
+                    caracteres_counter += 1
+        file.close()
+
+    for char, count in counters.items():
+        if count > 0:
+            print(char, '->', count)
+
+    
+    print(f'Caracteres numericos -> {numeros_counter}')
+    print(f'Caracteres especiales -> {caracteres_counter}')
+
 except IOError as e:
     print("Se produjo un error de E/S: ", strerror(e.errno))
