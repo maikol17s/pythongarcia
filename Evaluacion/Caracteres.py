5f2df50d74d50471a9240a3f32b391c6ffd61c5c
maikol17s <maikol8382007@gmail.com>
2023-08-02 10:11:37 -0500

    modulo 4

--- Evaluacion/Caracteres.py
+++ Evaluacion/Caracteres.py
@@ -4,7 +4,7 @@ def Caracteres(nombreArchivo):
         contenido = archivo.read()
         numCaracteres = len(contenido)
         archivo.close()
-        print(f'El archivo contiene {numCaracteres} caracteres.')
+        print(f'El archivo tiene {numCaracteres} caracteres.')
     except FileNotFoundError:
         print(f'El archivo {NombreArchivo} no existe.')
 
