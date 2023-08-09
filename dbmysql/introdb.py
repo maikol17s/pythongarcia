a7eb052de5668efc49f452c6ccbd4d74669261cd
maikol17s <maikol8382007@gmail.com>
2023-08-08 21:25:28 -0500

    MYSQL

--- dbmysql/introdb.py
+++ dbmysql/introdb.py
@@ -1,6 +1,6 @@
 import mysql.connect
 
-database = mysql.connector.connect(
+database = MYSQL.connect.connect(
     host='localhost',
     user='root',
     password='',
@@ -28,3 +28,39 @@ def insertar_empresa():
     print('Empresa creada con exito.')
 
 insertar_empresa()
+
+def Consultar():
+        valor = input('Ingrese el campo a consultar:')
+        consultar = f"SELECT {valor} FROM candidato;"
+        print(consultar)
+
+        cursor = database.cursor()
+        cursor.execute(consulta)
+        for dato in cursor:
+            print(dato)
+        cursor.close()
+
+def actualizar_empresa():
+        c1 = input('Ingrese el nombre del campo actual:')
+        d1 = input('Ingrese el dato nuevo del campo que eligió:')
+        c2 = input('Ingrese un campo relacionado con el dato a cambiar:')
+        d2 = input('Ingrese el dato del campo relacionado que eligió:')
+        
+        ModificarRegistro = f"UPDATE candidato SET {c1} = %s WHERE {c2} = %s;"
+        print(ModificarRegistro)
+
+        cursor = database.cursor()
+        cursor.execute(mfcarRegistro, (d1, d2))
+        database.commit()
+        cursor.close()
+
+def eliminarDatos():
+        cam = input('Ingrese el campo a consultar:')
+        dato = input('Ingrese el dato a eliminar:')
+        eliminar = f"DELETE FROM candidato WHERE {campo} = %s;"
+        print(eliminar)
+
+        cursor = database.cursor()
+        cursor.execute(eliminar, (dato,))
+        database.commit()
+        cursor.close()
